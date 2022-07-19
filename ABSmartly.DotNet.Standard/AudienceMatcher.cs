37467512854da38bf83f6027f804ad033059e576
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 14:48:44 +0200

    [Standard] AudienceMatcher: continued

--- ABSmartly.DotNet.Standard/AudienceMatcher.cs
+++ ABSmartly.DotNet.Standard/AudienceMatcher.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 using ABSmartly.DotNet.Nio.Charset;
 using ABSmartly.JsonExpressions;
 using ABSmartly.Temp;
@@ -16,13 +17,16 @@ public class AudienceMatcher : IAudienceDeserializer
 
     public Result Evaluate(string audience, Dictionary<string, object> attributes)
     {
-        byte[] bytes = audience.GetBytes(StandardCharsets.UTF_8);
+        var bytes = Encoding.UTF8.GetBytes(audience);
         var audienceMap = Deserialize(bytes, 0, bytes.Length);
 
         if (audienceMap is null)
             return null;
 
-        object filter = audienceMap.Get("filter");
+        if (!audienceMap.TryGetValue("filter", out var filter))
+            return null;
+
+        //object filter = audienceMap.Get("filter");
         // Todo: Tegu: Generic type won't work out of the box, Dictionary and List of what type to look for?
         if (filter.GetType() is typeof(Dictionary<>) || filter.GetType() is typeof(List<>))
         {
