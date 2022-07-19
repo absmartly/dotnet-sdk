4e1d4f0ac5bb0bf6e23462c5b9a492cd127552e4
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 14:44:30 +0200

    [Standard] StandardCharsets: added

--- ABSmartly.DotNet.Standard/DotNet/Nio/Charset/StandardCharsets.cs
+++ ABSmartly.DotNet.Standard/DotNet/Nio/Charset/StandardCharsets.cs
@@ -1,6 +1,10 @@
-﻿namespace ABSmartly.DotNet.Nio.Charset;
+﻿using System.Text;
+
+namespace ABSmartly.DotNet.Nio.Charset;
 
 public class StandardCharsets
 {
-    
+    //Todo: If ASCII is not good, than look for / implement a specific converter to: 'us_ascii'
+    public static Encoding US_ASCII => Encoding.ASCII;
+    public static Encoding UTF_8 => Encoding.UTF8;
 }
