d7d5fa04bf39fa3e268536b3e8880537b7b42e7a
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 15:26:08 +0200

    [Standard] Operators: continued

--- ABSmartly.DotNet.Standard/JsonExpressions/Operators/BinaryOperator.cs
+++ ABSmartly.DotNet.Standard/JsonExpressions/Operators/BinaryOperator.cs
@@ -1,6 +1,25 @@
-﻿namespace ABSmartly.JsonExpressions.Operators;
+﻿using System.Collections.Generic;
 
-public class BinaryOperator
+namespace ABSmartly.JsonExpressions.Operators;
+
+public abstract class BinaryOperator : IOperator
 {
-    
+    public object Evaluate(IEvaluator evaluator, object args)
+    {
+        if (args is not List<object> objectList)
+            return null;
+
+        var lhs = objectList.Count > 0 ? evaluator.Evaluate(objectList[0]) : null;
+        if (lhs is null)
+            return null;
+
+        // Todo: Tegu: review!?
+        var rhs = objectList.Count > 1 ? evaluator.Evaluate(objectList[1]) : null;
+        if (rhs is null)
+            return null;
+
+        return Binary(evaluator, lhs, rhs);
+    }
+
+    public abstract object Binary(IEvaluator evaluator, object lhs, object rhs);
 }
