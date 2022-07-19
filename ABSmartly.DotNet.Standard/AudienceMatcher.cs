998d20cc2e2d38f3097c472b0d8cd007a637e712
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 14:37:39 +0200

    [Standard] AudienceMatcher: started

--- ABSmartly.DotNet.Standard/AudienceMatcher.cs
+++ ABSmartly.DotNet.Standard/AudienceMatcher.cs
@@ -1,6 +1,39 @@
-﻿namespace ABSmartly;
+﻿using System.Collections.Generic;
+using ABSmartly.DotNet.Nio.Charset;
+using ABSmartly.JsonExpressions;
+using ABSmartly.Temp;
 
-public class AudienceMatcher
+namespace ABSmartly;
+
+public class AudienceMatcher : IAudienceDeserializer
 {
-    
+    private readonly JsonExpr _jsonExpr;
+
+    public AudienceMatcher()
+    {
+        _jsonExpr = new JsonExpr();
+    }
+
+    public Result Evaluate(string audience, Dictionary<string, object> attributes)
+    {
+        byte[] bytes = audience.GetBytes(StandardCharsets.UTF_8);
+        var audienceMap = Deserialize(bytes, 0, bytes.Length);
+
+        if (audienceMap is null)
+            return null;
+
+        object filter = audienceMap.Get("filter");
+        // Todo: Tegu: Generic type won't work out of the box, Dictionary and List of what type to look for?
+        if (filter.GetType() is typeof(Dictionary<>) || filter.GetType() is typeof(List<>))
+        {
+            return new Result(_jsonExpr.EvaluateBookeanExpr(filter, attributes));
+        }
+    }
+
+
+
+    public Dictionary<string, object> Deserialize(byte[] bytes, int offset, int length)
+    {
+        throw new System.NotImplementedException();
+    }
 }
---
+++ ABSmartly.DotNet.Standard/Temp/Result.cs
@@ -0,0 +1,11 @@
+﻿namespace ABSmartly.Temp;
+
+public class Result
+{
+    public bool Value { get; }
+
+    public Result(bool value)
+    {
+        Value = value;
+    }
+}
