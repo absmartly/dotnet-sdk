47607340697702c77ec2ffe191112725ca8bc831
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 15:02:00 +0200

    [Standard] JsonExpr: started

--- ABSmartly.DotNet.Standard/JsonExpressions/JsonExpr.cs
+++ ABSmartly.DotNet.Standard/JsonExpressions/JsonExpr.cs
@@ -1,6 +1,38 @@
-﻿namespace ABSmartly.JsonExpressions;
+﻿using System.Collections.Generic;
+using ABSmartly.JsonExpressions.Operators;
+
+namespace ABSmartly.JsonExpressions;
 
 public class JsonExpr
 {
-    
+    private static Dictionary<string, Operator> operators;
+
+    static JsonExpr()
+    {
+        operators = new Dictionary<string, Operator>();
+        operators.Add("and", new AndCombinator());
+        operators.Add("or", new OrCombinator());
+        operators.Add("var", new VarOperator());
+        operators.Add("null", new NullOperator());
+        operators.Add("not", new NotOperator());
+        operators.Add("in", new InOperator());
+        operators.Add("match", new MatchOperator());
+        operators.Add("eq", new EqualsOperator());
+        operators.Add("gt", new GreaterThanOperator());
+        operators.Add("gte", new GreaterThanOrEqualOperator());
+        operators.Add("lt", new LessThanOperator());
+        operators.Add("lte", new LessThanOrEqualOperator());
+    }
+
+    public bool EvaluateBooleanExpr(object expression, Dictionary<string, object> variables)
+    {
+        var evaluator = new ExprEvaluator(operators, variables);
+        return evaluator.BooleanConvert(evaluator.Evaluate(expression));
+    }
+
+    public object EvaluateExpression(object expression, Dictionary<string, object> variables)
+    {
+        var evaluator = new ExprEvaluator(operators, variables);
+        return evaluator.Evaluate(expression);
+    }
 }
