5b9f7924b99488a969b5ebdc76d31aa7e41981d6
Istvan Piroska <istvan@tegusolutions.com>
2022-07-19 21:20:59 +0200

    [Standard] Json Operators: first version is ready

--- ABSmartly.DotNet.Standard/JsonExpressions/Operators/InOperator.cs
+++ ABSmartly.DotNet.Standard/JsonExpressions/Operators/InOperator.cs
@@ -1,6 +1,35 @@
-﻿namespace ABSmartly.JsonExpressions.Operators;
+﻿using System.Collections.Generic;
 
-public class InOperator
+namespace ABSmartly.JsonExpressions.Operators;
+
+// Todo: Tegu: cleanup
+public class InOperator : BinaryOperator
 {
-    
+    public override object Binary(IEvaluator evaluator, object haystack, object needle)
+    {
+        if (haystack is List<object> list) 
+        {
+            foreach (var listitem in list)
+            {
+                if (evaluator.Compare(listitem, needle) == 0)
+                    return true;
+            }
+
+            return false;
+        }
+
+        if (haystack is string) 
+        {
+            var needleString = evaluator.StringConvert(needle);
+            return needleString != null && ((string) haystack).Contains(needleString);
+        }
+
+        if (haystack is Dictionary<string, object>) 
+        {
+            var needleString = evaluator.StringConvert(needle);
+            return needleString != null && ((Dictionary<string, object>) haystack).ContainsKey(needleString);
+        }
+
+        return null;
+    }
 }
--- ABSmartly.DotNet.Standard/JsonExpressions/Operators/MatchOperator.cs
+++ ABSmartly.DotNet.Standard/JsonExpressions/Operators/MatchOperator.cs
@@ -1,6 +1,30 @@
-﻿namespace ABSmartly.JsonExpressions.Operators;
+﻿using System;
 
-public class MatchOperator
+namespace ABSmartly.JsonExpressions.Operators;
+
+public class MatchOperator : BinaryOperator
 {
-    
+    // Todo: Tegu: cleanup
+    public override object Binary(IEvaluator evaluator, object lhs, object rhs)
+    {
+        var text = evaluator.StringConvert(lhs);
+        if (text == null) 
+            return null;
+        
+        var pattern = evaluator.StringConvert(rhs);
+        if (pattern == null) 
+            return null;
+            
+        try
+        {
+            var compiled = Pattern.Compile(pattern);
+            var matcher = compiled.matcher(text);
+            return matcher.find();
+        }
+        catch (Exception e)
+        {
+        }
+
+        return null;
+    }
 }
