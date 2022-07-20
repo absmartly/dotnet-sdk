f35f678d6af830fb7ae7cba5a610f42a00361f73
Istvan Piroska <istvan@tegusolutions.com>
2022-07-20 18:45:23 +0200

    [Standard] Hashing: started

--- ABSmartly.DotNet.Standard/Internal/Hashing/Hashing.cs
+++ ABSmartly.DotNet.Standard/Internal/Hashing/Hashing.cs
@@ -1,6 +1,34 @@
-﻿namespace ABSmartly.Internal.Hashing;
+﻿using System;
+using System.Threading;
 
-public class Hashing
+namespace ABSmartly.Internal.Hashing;
+
+public abstract class Hashing
 {
-    
+    protected Hashing()
+    {
+        
+    }
+    // Todo: ???
+    private static readonly Func<byte[]> valueFactory = () => new byte[512];
+    private static ThreadLocal<byte[]> threadBuffer = new(() => valueFactory());
+
+    public static byte[] HashUnit(char[] unit)
+    {
+        var n = unit.Length;
+        var bufferLength = n << 1;
+
+        var buffer = threadBuffer.Value;
+        if (buffer.Length < bufferLength)
+        {
+            //var bit = 32 - Integer.numberOfLeadingZeros(bufferLen - 1);
+            //buffer = new byte[1 << bit];
+            ////threadBuffer.set(buffer);
+            //threadBuffer.Value = buffer;
+        }
+
+        var encoded = Buffers.EncodeUTF8(buffer, 0, unit);
+        return MD5.DigestBase64UrlNoPadding(buffer, 0, encoded);
+    }
+
 }
