79cee7eb1b1efa01445b412f10b3eb50890981e3
Istvan Piroska <istvan@tegusolutions.com>
2022-07-21 16:10:34 +0200

    [Standard] Objects: added

--- ABSmartly.DotNet.Standard/DotNet/Util/Objects.cs
+++ ABSmartly.DotNet.Standard/DotNet/Util/Objects.cs
@@ -2,5 +2,34 @@
 
 public class Objects
 {
-    
+    public new static bool Equals(object a, object b) {
+        return (a == b) || (a != null && a.Equals(b));
+    }
+
+    // Todo: review, needed?
+
+    //public static int hash(Object... values) {
+    //    return Arrays.hashCode(values);
+    //}
+
+    public static int Hash()
+    {
+        return 0;
+    }
+
+    public static int Hash(object o1)
+    {
+        return 0;
+    }
+
+    public static int Hash(object o1, object o2)
+    {
+        return 0;
+    }
+
+    public static int Hash(object[] os)
+    {
+        return 0;
+    }
+
 }
