2769788f10de516731a5a92be417e69b2369e91d
Istvan Piroska <istvan@tegusolutions.com>
2022-07-21 16:31:14 +0200

    [Standard] GoalAchievement: added

--- ABSmartly.DotNet.Standard/Json/GoalAchievement.cs
+++ ABSmartly.DotNet.Standard/Json/GoalAchievement.cs
@@ -1,6 +1,57 @@
-﻿namespace ABSmartly.Json;
+﻿using System.Collections.Generic;
 
+namespace ABSmartly.Json;
+
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class GoalAchievement
 {
-    
+    public GoalAchievement()
+    {
+        
+    }
+
+    public GoalAchievement(string name, long achievedAt, Dictionary<string, object> properties)
+    {
+        Name = name;
+        AchievedAt = achievedAt;
+        Properties = properties;
+    }
+
+    public string Name { get; set; }
+    public long AchievedAt { get; set; }
+    public Dictionary<string, object> Properties { get; set; }
+
+    protected bool Equals(GoalAchievement other)
+    {
+        return Name == other.Name && AchievedAt == other.AchievedAt && Equals(Properties, other.Properties);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((GoalAchievement)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = (Name != null ? Name.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ AchievedAt.GetHashCode();
+            hashCode = (hashCode * 397) ^ (Properties != null ? Properties.GetHashCode() : 0);
+            return hashCode;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "GoalAchievement{" +
+               "name='" + Name + '\'' +
+               ", achievedAt=" + AchievedAt +
+               ", properties=" + Properties +
+               '}';
+    }
 }
