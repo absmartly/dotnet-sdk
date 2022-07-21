7f7b16ef8888ac589fcf1ec40e61c9431e57f554
Istvan Piroska <istvan@tegusolutions.com>
2022-07-21 16:36:22 +0200

    [Standard] PublishEvent: added

--- ABSmartly.DotNet.Standard/Json/PublishEvent.cs
+++ ABSmartly.DotNet.Standard/Json/PublishEvent.cs
@@ -1,6 +1,68 @@
 ﻿namespace ABSmartly.Json;
 
+//@JsonInclude(JsonInclude.Include.NON_NULL)
+//@JsonIgnoreProperties(ignoreUnknown = true)
 public class PublishEvent
 {
-    
+    public PublishEvent()
+    {
+        
+    }
+
+    public PublishEvent(bool hashed, Unit[] units, long publishedAt, Exposure[] exposures, GoalAchievement[] goals, Attribute[] attributes)
+    {
+        Hashed = hashed;
+        Units = units;
+        PublishedAt = publishedAt;
+        Exposures = exposures;
+        Goals = goals;
+        Attributes = attributes;
+    }
+
+    public bool Hashed { get; set; }
+    public Unit[] Units { get; set; }
+    public long PublishedAt { get; set; }
+    public Exposure[] Exposures { get; set; }
+    public GoalAchievement[] Goals { get; set; }
+    public Attribute[] Attributes { get; set; }
+
+
+    protected bool Equals(PublishEvent other)
+    {
+        return Hashed == other.Hashed && Equals(Units, other.Units) && PublishedAt == other.PublishedAt && Equals(Exposures, other.Exposures) && Equals(Goals, other.Goals) && Equals(Attributes, other.Attributes);
+    }
+
+    public override bool Equals(object obj)
+    {
+        if (ReferenceEquals(null, obj)) return false;
+        if (ReferenceEquals(this, obj)) return true;
+        if (obj.GetType() != this.GetType()) return false;
+        return Equals((PublishEvent)obj);
+    }
+
+    public override int GetHashCode()
+    {
+        unchecked
+        {
+            var hashCode = Hashed.GetHashCode();
+            hashCode = (hashCode * 397) ^ (Units != null ? Units.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ PublishedAt.GetHashCode();
+            hashCode = (hashCode * 397) ^ (Exposures != null ? Exposures.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ (Goals != null ? Goals.GetHashCode() : 0);
+            hashCode = (hashCode * 397) ^ (Attributes != null ? Attributes.GetHashCode() : 0);
+            return hashCode;
+        }
+    }
+
+    public override string ToString()
+    {
+        return "PublishEvent{" +
+               "hashedUnits=" + Hashed +
+               //", units=" + Arrays.toString(Units) +
+               ", publishedAt=" + PublishedAt +
+               //", exposures=" + Arrays.toString(Exposures) +
+               //", goals=" + Arrays.toString(Goals) +
+               //", attributes=" + Arrays.toString(Attributes) +
+               '}';
+    }
 }
