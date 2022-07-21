9d7e26ca67be4152fc16c7cc5348edd836dcfad1
Istvan Piroska <istvan@tegusolutions.com>
2022-07-21 14:03:51 +0200

    [Standard] Algorithm: added

--- ABSmartly.DotNet.Standard/Internal/Algorithm.cs
+++ ABSmartly.DotNet.Standard/Internal/Algorithm.cs
@@ -1,6 +1,32 @@
-﻿namespace ABSmartly.Internal;
+﻿using System;
+using System.Collections.Generic;
+
+namespace ABSmartly.Internal;
 
 public class Algorithm
 {
-    
+    public static R[] MapSetToArray<T, R>(HashSet<T> set, R[] array, Func<T, R> mapper) 
+    {
+        var size = set.Count;
+
+        if (array.Length < size) 
+        {
+            //array = (R[]) java.lang.reflect.Array.newInstance(array.getClass().getComponentType(), size);
+            array = new R[size];
+        }
+
+        if (array.Length > size) 
+        {
+            //array[size] = null;
+            array[size] = default(R);
+        }
+
+        int index = 0;
+
+
+        foreach (var value in set) {
+            array[index++] = mapper.Invoke(value);
+        }
+        return array;
+    }
 }
