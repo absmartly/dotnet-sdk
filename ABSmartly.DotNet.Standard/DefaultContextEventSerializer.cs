9a1177b77ade956f386084373ca99494d5fbb91d
Istvan Piroska <istvan@tegusolutions.com>
2022-07-22 19:50:42 +0200

    [Standard] DefaultContextEventSerializer: added

--- ABSmartly.DotNet.Standard/DefaultContextEventSerializer.cs
+++ ABSmartly.DotNet.Standard/DefaultContextEventSerializer.cs
@@ -1,6 +1,30 @@
-﻿namespace ABSmartly;
+﻿using System;
+using System.Text.Json;
+using ABSmartly.Json;
+using Microsoft.Extensions.Logging;
 
-public class DefaultContextEventSerializer
+namespace ABSmartly;
+
+public class DefaultContextEventSerializer : IContextEventSerializer
 {
-    
+    private readonly ILogger<DefaultContextEventSerializer> _logger;
+
+    public DefaultContextEventSerializer(ILogger<DefaultContextEventSerializer> logger)
+    {
+        _logger = logger;
+    }
+
+    public byte[] Serialize(PublishEvent publishEvent)
+    {
+        try
+        {
+            // Todo: ObjectMapper.writeValueAsBytes ??
+            return JsonSerializer.SerializeToUtf8Bytes(publishEvent);
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return null;
+        }
+    }
 }
