45a15329ea7566922eeff0a306731bfb66322ce7
Istvan Piroska <istvan@tegusolutions.com>
2022-07-22 19:27:06 +0200

    [Standard] DefaultVariableParser: started

--- ABSmartly.DotNet.Standard/DefaultVariableParser.cs
+++ ABSmartly.DotNet.Standard/DefaultVariableParser.cs
@@ -1,6 +1,30 @@
-﻿namespace ABSmartly;
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.Extensions.Logging;
 
-public class DefaultVariableParser
+namespace ABSmartly;
+
+public class DefaultVariableParser : IVariableParser
 {
-    
+    private readonly ILogger<DefaultVariableParser> _logger;
+
+    // Todo: add the com.fasterxml.jackson.databind C# (Jackson.Core?) package???? or use simple JSON
+
+    public DefaultVariableParser(ILogger<DefaultVariableParser> logger)
+    {
+        _logger = logger;
+    }
+
+    public Dictionary<string, object> Parse(Context context, string experimentName, string variantName, string variableValue)
+    {
+        try
+        {
+            throw new NotImplementedException();
+        }
+        catch (Exception e)
+        {
+            _logger.LogError(e.Message);
+            return null;
+        }
+    }
 }
