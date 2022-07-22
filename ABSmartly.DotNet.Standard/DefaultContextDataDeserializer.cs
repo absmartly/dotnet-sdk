a005e6e556780d1c0c3842387251252817c1c842
Istvan Piroska <istvan@tegusolutions.com>
2022-07-22 19:53:48 +0200

    [Standard] DefaultContextDataDeserializer: added

--- ABSmartly.DotNet.Standard/DefaultContextDataDeserializer.cs
+++ ABSmartly.DotNet.Standard/DefaultContextDataDeserializer.cs
@@ -1,6 +1,20 @@
-﻿namespace ABSmartly;
+﻿using System.Threading.Tasks;
+using ABSmartly.Json;
 
-public class DefaultContextDataDeserializer
+namespace ABSmartly;
+
+public class DefaultContextDataDeserializer : IContextEventHandler
 {
-    
+    private readonly Client _client;
+
+    public DefaultContextDataDeserializer(Client client)
+    {
+        _client = client;
+    }
+
+
+    public async Task PublishAsync(Context context, PublishEvent publishEvent)
+    {
+        return await _client.Publish(publishEvent);
+    }
 }
