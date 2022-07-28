9e95931e0d1ef0c63bc265b807ea3d509e035ea5
Istvan Piroska <istvan@tegusolutions.com>
2022-07-28 13:46:05 +0200

    [Standard] DefaultHttpClient implementation started

--- ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultHTTPClient.cs
+++ ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultHTTPClient.cs
@@ -1,16 +1,16 @@
-﻿using ABSmartly.Interfaces;
+﻿using System;
+using ABSmartly.Interfaces;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Threading.Tasks;
 
 namespace ABSmartly.DefaultServiceImplementations;
 
-internal class DefaultHttpClient : IHttpClient
+internal class DefaultHttpClient : IHttpClient, IDisposable
 {
+    private IHttpClientFactory _httpClientFactory;
+
     #region Lifecycle
-
-    
-
-    #endregion
 
     public DefaultHttpClient(DefaultHttpClientConfig config)
     {
@@ -20,11 +20,24 @@ internal class DefaultHttpClient : IHttpClient
     public static DefaultHttpClient Create(DefaultHttpClientConfig config)
     {
         return new DefaultHttpClient(config);
-    }
+    }    
+
+    #endregion
+
+
 
 
     public async Task<IResponse> GetAsync(string url, Dictionary<string, string> query, Dictionary<string, string> headers)
     {
+        var httpClient = _httpClientFactory.CreateClient();
+
+        // Step 1: Buiild the request
+        httpClient.DefaultRequestHeaders.Add("", query);
+
+        foreach (var kvp in headers)
+            httpClient.DefaultRequestHeaders.Add(kvp.Key, kvp.Value);
+        
+
 
         throw new System.NotImplementedException();
     }
@@ -40,10 +53,24 @@ internal class DefaultHttpClient : IHttpClient
     }
 
 
+
+    private void BuildRequest(HttpClient httpClient, Dictionary<string, string> query, Dictionary<string, string> headers, byte[] body)
+    {
+        HttpClient httpClient;
+        httpClient = new HttpClient();
+        httpClient.re
+    }
+
+
+
+    #region IDisposable
+
     public void Dispose()
     {
         throw new System.NotImplementedException();
-    }
+    }    
+
+    #endregion
 }
 
 public class DefaultResponse : IResponse
