2e8717bc4c4acb3a90974c75f5e9c8456ba0a54c
Istvan Piroska <istvan@tegusolutions.com>
2022-07-28 22:34:34 +0200

    [Standard.Tests] ABSmartly.DotNet.Standard.UnitTests project added with NUnit 3

---
+++ ABSmartly.DotNet.Standard.UnitTests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace ABSmartly.DotNet.Standard.UnitTests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
