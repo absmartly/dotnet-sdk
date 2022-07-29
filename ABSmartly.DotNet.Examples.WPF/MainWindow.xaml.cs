9913f5469cb1eb185b8ab2ca78c23e771ecd7bca
Istvan Piroska <istvan@tegusolutions.com>
2022-07-29 17:48:36 +0200

    [Examples.WPF] Application started (namespaces needs to be fixed and updated), SDK init and Button click track example added

--- ABSmartly.DotNet.Examples.WPF/MainWindow.xaml.cs
+++ ABSmartly.DotNet.Examples.WPF/MainWindow.xaml.cs
@@ -1,28 +1,83 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Reflection;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ABSmartly;
 
-namespace ABSmartly.DotNet.Examples.WPF
+namespace WPF
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
     /// </summary>
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, IDisposable
     {
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private ABSmartly.ABSmartly sdk;
+        private Dictionary<string, object> properties;
+        private Context context;
+
+        private void InitializeABSmartly()
+        {
+            ClientConfig config = new ClientConfig(
+                endpoint: "https://acme.absmartly.io/v1",
+                apiKey: "apiKey",
+                application: ProductName,
+                environment: "Development"
+            );
+
+            ABSmartlyConfig sdkConfig = ABSmartlyConfig.Create();
+
+            sdk = ABSmartly.ABSmartly.Create(sdkConfig);
+
+            ContextConfig contextConfig = ContextConfig.Create()
+                .SetUnit("session_id", "bf06d8cb5d8137290c4abb64155584fbdb64d8")
+                .SetUnit("user_id", "123456");
+
+            context = sdk.CreateContext(contextConfig).WaitUntilReady();
+            int treatment = context.GetTreatment("exp_test_ab");
+            Debug.WriteLine(treatment);
+
+            properties = new Dictionary<string, object>
+            {
+                { "value", 125 },
+                { "fee", 125 }
+            };
+        }
+
+        private void Button1_Click(object sender, RoutedEventArgs e)
+        {
+
+
+            context.Track("payment", properties);
+
+
+        }
+
+
+        #region Helper
+
+        public static string ProductName
+        {
+            get
+            {
+                AssemblyProductAttribute myProduct =
+                    (AssemblyProductAttribute)AssemblyProductAttribute.GetCustomAttribute(Assembly.GetExecutingAssembly(),
+                        typeof(AssemblyProductAttribute));
+                return myProduct.Product;
+            }
+        }
+
+        #endregion
+
+        public void Dispose()
+        {
+            context.Close();
+            sdk.Close();            
         }
     }
 }
