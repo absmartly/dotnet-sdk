b7e1f26eb549e699416d3762e4659c4609724500
Istvan Piroska <istvan@tegusolutions.com>
2022-07-30 22:04:58 +0200

    [Standard.Tests] ConcurrencyTests - ComputeIfAbsentRW started

---
+++ ABSmartly.DotNet.Standard.UnitTests/InternalTests/ConcurrencyTests.cs
@@ -0,0 +1,18 @@
+﻿using Moq;
+
+namespace ABSmartly.DotNet.Standard.UnitTests.InternalTests;
+
+[TestFixture]
+public class ConcurrencyTests
+{
+    public void ComputeIfAbsentRW()
+    {
+        var map = new Mock<Dictionary<int, int>>();
+        var computer = new Mock<Func<int, int>>();
+        var rlock = new Mock<ReaderWriterLockSlim>();
+        var wlock = new Mock<ReaderWriterLockSlim>();
+        var rwlock = new Mock<ReaderWriterLockSlim>();
+
+        //rwlock.Setup(p => p.EnterReadLock()).Returns(rlock);
+    }
+}
