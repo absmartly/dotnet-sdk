9b59c2a426e13740c13116c99a1a75c083e14f47
Istvan Piroska <istvan@tegusolutions.com>
2022-08-05 18:01:17 +0200

    [Standard.Tests, Standard] InOperator: fixed and refactored for better testability

--- ABSmartly.DotNet.Standard/JsonExpressions/Operators/InOperator.cs
+++ ABSmartly.DotNet.Standard/JsonExpressions/Operators/InOperator.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System.Collections;
+using System.Collections.Generic;
 
 namespace ABSmartly.JsonExpressions.Operators;
 
@@ -7,29 +8,74 @@ public class InOperator : BinaryOperator
 {
     public override object Binary(IEvaluator evaluator, object haystack, object needle)
     {
-        if (haystack is List<object> list) 
+        if (haystack is string hsString)
         {
-            foreach (var listitem in list)
-            {
-                if (evaluator.Compare(listitem, needle) == 0)
-                    return true;
-            }
+            return HandleString(evaluator, hsString, needle);
+            //var needleString = evaluator.StringConvert(needle);
+            ////return needleString != null && hsString.Contains(needleString);
 
-            return false;
+            ////if (needleString == null) 
+            //if (string.IsNullOrWhiteSpace(needleString))
+            //    return false;
+
+            //return hsString.Contains(needleString);
         }
 
-        if (haystack is string) 
+        if (haystack is IList hsList)
         {
-            var needleString = evaluator.StringConvert(needle);
-            return needleString != null && ((string) haystack).Contains(needleString);
+            return HandleIList(evaluator, hsList, needle);
+            //foreach (var listitem in list)
+            //{
+            //    if (evaluator.Compare(listitem, needle) == 0)
+            //        return true;
+            //}
+
+            //return false;
         }
 
-        if (haystack is Dictionary<string, object>) 
+
+
+        if (haystack is IDictionary<string, object> hsDictionary)
         {
-            var needleString = evaluator.StringConvert(needle);
-            return needleString != null && ((Dictionary<string, object>) haystack).ContainsKey(needleString);
+            return HandleIDictionary(evaluator, hsDictionary, needle);
+
+            //var needleString = evaluator.StringConvert(needle);
+            //if (needleString == null) 
+            //    return false;
+            //return hsDictionary.ContainsKey(needleString);
         }
 
         return null;
+    }
+
+    private static object HandleString(IEvaluator evaluator, string hsString, object needle)
+    {
+        var needleString = evaluator.StringConvert(needle);
+
+        if (string.IsNullOrWhiteSpace(needleString))
+            return false;
+
+        return hsString.Contains(needleString);
+    }
+
+    private static object HandleIList(IEvaluator evaluator, IList hsList, object needle)
+    {
+        foreach (var hsListItem in hsList)
+        {
+            if (evaluator.Compare(hsListItem, needle) == 0)
+                return true;
+        }
+
+        return false;
+    }
+
+    private static object HandleIDictionary(IEvaluator evaluator, IDictionary<string, object> hsDictionary, object needle)
+    {
+        //var needleString = evaluator.StringConvert(needle);
+        //return needleString != null && ((Dictionary<string, object>) haystack).ContainsKey(needleString);
+        var needleString = evaluator.StringConvert(needle);
+        if (string.IsNullOrWhiteSpace(needleString)) 
+            return false;
+        return hsDictionary.ContainsKey(needleString);
     }
 }
