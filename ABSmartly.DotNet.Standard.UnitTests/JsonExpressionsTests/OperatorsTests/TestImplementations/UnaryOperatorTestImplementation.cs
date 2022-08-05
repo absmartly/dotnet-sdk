382d16f8f628bc49f4abeb06c5967b4f0ad8d66b
Istvan Piroska <istvan@tegusolutions.com>
2022-08-05 21:11:00 +0200

    [Standard.Tests] UnaryOperatorTestImplementation: created UnaryOperatorTests added: - Evaluate_Returns_ValidResult

---
+++ ABSmartly.DotNet.Standard.UnitTests/JsonExpressionsTests/OperatorsTests/TestImplementations/UnaryOperatorTestImplementation.cs
@@ -0,0 +1,14 @@
+﻿using ABSmartly.JsonExpressions;
+using ABSmartly.JsonExpressions.Operators;
+
+namespace ABSmartly.DotNet.Standard.UnitTests.JsonExpressionsTests.OperatorsTests.TestImplementations;
+
+public class UnaryOperatorTestImplementation : UnaryOperator
+{
+    public static object ValidResult => "Unary Pass";
+
+    public override object Unary(IEvaluator evaluator, object arg)
+    {
+        return ValidResult;
+    }
+}
