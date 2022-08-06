59730c373eed9b967554cba0a962fe4b4d11f6bc
Istvan Piroska <istvan@tegusolutions.com>
2022-08-06 18:33:13 +0200

    [Standard.Tests] ValueOperatorTests added: - Evaluate_Returns_Parameter

---
+++ ABSmartly.DotNet.Standard.UnitTests/JsonExpressionsTests/OperatorsTests/ValueOperatorTests.cs
@@ -0,0 +1,26 @@
+﻿using ABSmartly.DotNet.Standard.UnitTests.TestUtils;
+using ABSmartly.JsonExpressions.Operators;
+
+namespace ABSmartly.DotNet.Standard.UnitTests.JsonExpressionsTests.OperatorsTests;
+
+[TestFixture]
+public class ValueOperatorTests : TestCases
+{
+#pragma warning disable CS8618
+    private ValueOperator valueOperator;
+#pragma warning restore CS8618
+
+    [SetUp]
+    public void Init()
+    {
+        valueOperator = new ValueOperator();
+    }
+
+    [TestCaseSource(nameof(RandomNotIListValues))]
+    public void Evaluate_Returns_Parameter(object parameter)
+    {
+        var result = valueOperator.Evaluate(null, parameter);
+
+        Assert.That(result, Is.EqualTo(parameter));
+    }
+}
