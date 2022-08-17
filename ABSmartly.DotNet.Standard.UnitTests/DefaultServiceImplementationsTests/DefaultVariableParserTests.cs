093199ada5ab55c2f8685a0f6fa4c7c45f6531f3
Istvan Piroska <istvan@tegusolutions.com>
2022-08-17 20:02:01 +0200

    [Standard.Tests] DefaultVariableParserTests: started I am facing with unexpected issues with Json deserialization into object (Dictionary<string, object>).. The object is used as JsonElement types, instead of the value.

--- ABSmartly.DotNet.Standard.UnitTests/DefaultServiceImplementationsTests/DefaultVariableParserTests.cs
+++ ABSmartly.DotNet.Standard.UnitTests/DefaultServiceImplementationsTests/DefaultVariableParserTests.cs
@@ -1,6 +1,19 @@
-﻿namespace ABSmartly.DotNet.Standard.UnitTests.DefaultServiceImplementationsTests;
+﻿using System.Text.Json;
+using ABSmartly.DefaultServiceImplementations;
+using ABSmartly.DotNet.Standard.UnitTests.TestUtils;
 
+namespace ABSmartly.DotNet.Standard.UnitTests.DefaultServiceImplementationsTests;
+
+[TestFixture]
 public class DefaultVariableParserTests
 {
-    
+    [Test]
+    public void Test1()
+    {
+        var parser = new DefaultVariableParser();
+
+        var resultDictionary = parser.Parse(null, null, null, TestData.Json.Variables);
+
+        Assert.That(((JsonElement)resultDictionary["a"]).GetInt32(), Is.EqualTo(1));
+    }
 }
--- ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultVariableParser.cs
+++ ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultVariableParser.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text.Json;
+using System.Text.Json.Nodes;
 using ABSmartly.Interfaces;
 using Microsoft.Extensions.Logging;
 
@@ -12,21 +13,43 @@ public class DefaultVariableParser : IVariableParser
 
     // Todo: add the com.fasterxml.jackson.databind C# (Jackson.Core?) package???? or use simple JSON
 
-    public DefaultVariableParser(ILoggerFactory loggerFactory)
+    public DefaultVariableParser(ILoggerFactory loggerFactory = null)
     {
-        _logger = loggerFactory.CreateLogger<DefaultVariableParser>();
+        _logger = loggerFactory?.CreateLogger<DefaultVariableParser>();
     }
 
     public Dictionary<string, object> Parse(Context context, string experimentName, string variantName, string config)
     {
         try
         {
+
+
+            var jsonNode = JsonNode.Parse(config);
+            //var array = jsonNode!.AsArray();
+
+            var resultDict = new Dictionary<string, object>();
+
+
+
+
+            var rerr = jsonNode.Deserialize<Dictionary<string, object>>();
+            //foreach (var VARIABLE in result.Deserialize<>())
+            //{
+                
+            //}
+
+
+
+            var data2 = JsonSerializer.Deserialize<JsonNode>(config);
+
+            
+            var data22 = JsonSerializer.Deserialize<Dictionary<string, dynamic>>(config);
             var data = JsonSerializer.Deserialize<Dictionary<string, object>>(config);
             return data;
         }
         catch (Exception e)
         {
-            _logger.LogError(e.Message);
+            _logger?.LogError(e.Message);
             return null;
         }
     }
