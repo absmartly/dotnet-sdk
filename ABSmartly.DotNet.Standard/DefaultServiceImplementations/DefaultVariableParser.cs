4e4192deca153416c7c7093a1e5f5d8426b9c3ae
Istvan Piroska <istvan@tegusolutions.com>
2022-08-18 19:18:41 +0200

    [Standard] JsonUtils class added with ParseJsonObject DefaultVariableParser: cleaned up

--- ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultVariableParser.cs
+++ ABSmartly.DotNet.Standard/DefaultServiceImplementations/DefaultVariableParser.cs
@@ -1,18 +1,15 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
 using ABSmartly.Interfaces;
+using ABSmartly.Utils.NewtonsoftJsonUtils;
 using Microsoft.Extensions.Logging;
 using Newtonsoft.Json;
-using Newtonsoft.Json.Linq;
 
 namespace ABSmartly.DefaultServiceImplementations;
 
 public class DefaultVariableParser : IVariableParser
 {
     private readonly ILogger<DefaultVariableParser> _logger;
-
-    // Todo: add the com.fasterxml.jackson.databind C# (Jackson.Core?) package???? or use simple JSON
 
     public DefaultVariableParser(ILoggerFactory loggerFactory = null)
     {
@@ -24,154 +21,13 @@ public class DefaultVariableParser : IVariableParser
         try
         {
             var jsonDict = JsonConvert.DeserializeObject<Dictionary<string, object>>(config);
-            var parsedDict = ParseJsonObject(jsonDict);
+            var parsedDict = JsonUtils.ParseJsonObject(jsonDict);
             return parsedDict as Dictionary<string, object>;
-
-            //var jArrayDict = new Dictionary<string, object>();
-            //var jObjectDict = new Dictionary<string, Dictionary<string, object>>();
-
-            //foreach (var kvp in jsonDict!)
-            //{
-            //    if (kvp.Value is JArray jArray)
-            //    {
-            //        var list = new List<object>();
-
-            //        foreach (var jItem in jArray)
-            //        {
-            //            list.Add(jItem.ToObject<object>());
-            //        }
-
-            //        jArrayDict.Add(kvp.Key, list);
-            //    }
-
-            //    else if (kvp.Value is JObject jObject)
-            //    {
-
-            //        var dict = new Dictionary<string, object>();
-            //        foreach (var jObjectKvP in jObject)
-            //        {
-            //            dict.Add(jObjectKvP.Key, jObjectKvP.Value.ToObject<object>());
-            //        }
-
-            //        jObjectDict.Add(kvp.Key, dict);
-            //    }
-            //}
-
-            //foreach (var kvp in jArrayDict)
-            //{
-            //    jsonDict[kvp.Key] = kvp.Value;
-            //}
-
-            //foreach (var kvp in jObjectDict)
-            //{
-            //    jsonDict[kvp.Key] = kvp.Value;
-            //}
-
-            //return jsonDict;
         }
         catch (Exception e)
         {
             _logger?.LogError(e.Message);
             return null;
         }
-    }
-
-    private object ParseJsonObject(object json)
-    {
-        //var jArrayDict = new Dictionary<string, object>();
-        //var jObjectDict = new Dictionary<string, Dictionary<string, object>>();
-
-        if (json is Dictionary<string, object> jsonDict)
-        {
-            for (var i = 0; i < jsonDict.Count; i++)
-            {
-
-
-                var kvpKey = jsonDict.ElementAt(i).Key;
-
-                if (kvpKey is "f")
-                {
-
-                }
-
-                var kvpValue = ParseJsonObject(jsonDict.ElementAt(i).Value);
-                jsonDict[kvpKey] = kvpValue;
-            }
-
-            return jsonDict;
-        }
-
-        if (json is JArray jArray)
-        {
-            var list = new List<object>();
-
-            foreach (var jArrayItem in jArray)
-            {
-                var parsedJArrayItem = ParseJsonObject(jArrayItem);
-                list.Add(parsedJArrayItem);
-            }
-
-            return list;
-        }
-
-        if (json is JObject jObject)
-        {
-            var dict = new Dictionary<string, object>();
-
-            foreach (var jObjectKvP in jObject)
-            {
-                dict.Add(jObjectKvP.Key, ParseJsonObject(jObjectKvP.Value.ToObject<object>()));
-            }
-
-            //jObjectDict.Add(kvp.Key, dict);
-
-            return dict;
-        }
-
-        if (json is JValue jValue)
-        {
-            return jValue.Value;
-        }
-
-        var type = json.GetType();
-        return json;
-
-
-        //foreach (var kvp in jsonDict!)
-        //{
-        //    if (kvp.Value is JArray jArray)
-        //    {
-        //        var list = new List<object>();
-        //        foreach (var jItem in jArray)
-        //        {
-        //            list.Add(jItem.ToObject<object>());
-        //        }
-
-        //        jArrayDict.Add(kvp.Key, list);
-        //    }
-
-        //    else if (kvp.Value is JObject jObject)
-        //    {
-        //        var dict = new Dictionary<string, object>();
-        //        foreach (var jObjectKvP in jObject)
-        //        {
-        //            dict.Add(jObjectKvP.Key, jObjectKvP.Value.ToObject<object>());
-        //        }
-
-        //        jObjectDict.Add(kvp.Key, dict);
-        //    }
-        //}
-
-        //foreach (var kvp in jArrayDict)
-        //{
-        //    jsonDict[kvp.Key] = kvp.Value;
-        //}
-
-        //foreach (var kvp in jObjectDict)
-        //{
-        //    jsonDict[kvp.Key] = kvp.Value;
-        //}
-
-        //return jsonDict;
     }
 }
---
+++ ABSmartly.DotNet.Standard/Utils/NewtonsoftJsonUtils/JsonUtils.cs
@@ -0,0 +1,63 @@
+﻿using Newtonsoft.Json.Linq;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ABSmartly.Utils.NewtonsoftJsonUtils;
+
+public class JsonUtils
+{
+    public static object ParseJsonObject(object json)
+    {
+        if (json is Dictionary<string, object> jsonDict)
+            return ParseDictionaryOfStringObject(jsonDict);
+
+        if (json is JArray jArray)
+            return ParseJArray(jArray);
+
+        if (json is JObject jObject)
+            return ParseJObject(jObject);
+
+        if (json is JValue jValue)
+            return jValue.Value;
+
+        return json;
+    }
+
+    private static object ParseDictionaryOfStringObject(IDictionary<string, object> jsonDict)
+    {
+        for (var i = 0; i < jsonDict.Count; i++)
+        {
+            var kvpKey = jsonDict.ElementAt(i).Key;
+
+            var kvpValue = ParseJsonObject(jsonDict.ElementAt(i).Value);
+            jsonDict[kvpKey] = kvpValue;
+        }
+
+        return jsonDict;
+    }
+
+    private static object ParseJArray(JArray jArray)
+    {
+        var list = new List<object>();
+
+        foreach (var jArrayItem in jArray)
+        {
+            var parsedJArrayItem = ParseJsonObject(jArrayItem);
+            list.Add(parsedJArrayItem);
+        }
+
+        return list;
+    }
+
+    private static object ParseJObject(JObject jObject)
+    {
+        var dict = new Dictionary<string, object>();
+
+        foreach (var jObjectKvP in jObject)
+        {
+            dict.Add(jObjectKvP.Key, ParseJsonObject(jObjectKvP.Value?.ToObject<object>()));
+        }
+
+        return dict;
+    }
+}
